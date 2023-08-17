8b291f186fe0d1f84991bdd42cc58e86d0944665
Artem Trunilin <a.trunilin92@mail.ru>
2023-08-17 16:46:27 +0300

    edits

--- test/confest.py
+++ test/confest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_managment():
     browser.config.base_url = ('https://demoqa.com/automation-practice-form')
-    browser.config.window_width = 1400
-    browser.config.window_height = 1600
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield
 
